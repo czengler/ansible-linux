3356084f6b3543e2a93f16ba90274ad7f1b0cb3f
Christoph Zengler <christoph.zengler@booleworks.com>
2022-12-26 17:40:38 +0100

    Also create new super user in install script

--- install_focal_bullseye.sh
+++ install_focal_bullseye.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-# Install ansible on Ubuntu Focal or Debian Bullseye
+# USAGE: ./install_focal_bullseye.sh [new_superuser]
+
+# Installs ansible, creates a new super user, and checks out my ansible files.
+# Intended for Ubuntu Focal and Debial Bullseye.
+
+# Install Ansible
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" >> /etc/apt/sources.list
-apt install gnupg2
+apt install -y gnupg2
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt update
-apt install ansible
+apt install -y ansible
+
+# Create new super user
+adduser "$USER"
+usermod -aG sudo "$USER"
+
+# Checkout ansible files for new user
+apt install -y git
+su - "$USER" -c 'git clone https://github.com/czengler/ansible-linux.git ansible'
 
