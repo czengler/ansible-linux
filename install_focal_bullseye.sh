eed913c270e029f24db4c1c307584b05408ef635
Christoph Zengler <christoph.zengler@booleworks.com>
2022-12-26 18:30:41 +0100

    Fixes for the first ansible task

--- install_focal_bullseye.sh
+++ install_focal_bullseye.sh
@@ -7,7 +7,7 @@
 
 # Install Ansible
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" >> /etc/apt/sources.list
-apt install -y gnupg2
+apt install -y gnupg2 sudo
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt update
 apt install -y ansible
@@ -20,3 +20,5 @@ usermod -aG sudo "$USER"
 apt install -y git
 su - "$USER" -c 'git clone https://github.com/czengler/ansible-linux.git ansible'
 
+# After that, the ansible playbook can be executed with the following command
+# ansible-playbook -K local.yml
