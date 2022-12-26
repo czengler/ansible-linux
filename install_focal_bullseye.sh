1ff9309b3648ddeae76b8d4e5fbc7eb2eb779c37
Christoph Zengler <christoph.zengler@booleworks.com>
2022-12-26 16:10:45 +0100

    Install gnupg before ansible

--- install_focal_bullseye.sh
+++ install_focal_bullseye.sh
@@ -2,6 +2,7 @@
 
 # Install ansible on Ubuntu Focal or Debian Bullseye
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" >> /etc/apt/sources.list
+apt install gnupg2
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt update
 apt install ansible
