171b44470bac03c7b8c5a397167d7e030a712926
Christoph Zengler <christoph.zengler@booleworks.com>
2022-12-26 14:57:20 +0100

    Install ansible on Ubuntu Focal / Debian Bullseye

---
+++ install_focal_bullseye.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install ansible on Ubuntu Focal or Debian Bullseye
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+apt update
+apt install ansible
+
